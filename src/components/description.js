0744caf8ace80875b9872d06f9604a3f78582f64
User <user@Users-MacBook-Pro.local>
2021-10-13 11:04:43 +0200

    description word change

--- src/components/description.js
+++ src/components/description.js
@@ -3,7 +3,7 @@ import classes from "./description.module.css";
 const Description = () => {
     return <div className={classes.card}>
         <p className={classes.text}>
-            To see your random advice, click on the "Click me" button.
+            To change your random advice, click on the "Click me" button.
         </p>
     </div>
 };
