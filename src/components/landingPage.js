60f2b3021b0571bbcd66f95a60a0fed0e36d5fbb
User <user@Users-MacBook-Pro.local>
2021-10-21 09:12:02 +0200

    removed commentted code (landing page)

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -18,10 +18,12 @@ const LandingPage = () => {
       <div className={classes.main}>
         <div className={classes.landingActions}>
           <h1 className={classes.title}>
-            Random <span className={classes.titleMobile}>Advice,</span> <span className={classes.titleMobile}>Jokes</span> and <span className={classes.titleMobile}>Cat Fact</span> Generator
+            Random <span className={classes.titleMobile}>Advice,</span>{" "}
+            <span className={classes.titleMobile}>Jokes</span> and{" "}
+            <span className={classes.titleMobile}>Cat Fact</span> Generator
           </h1>
           <p className={classes.desc}>Just something to pass the time.</p>
-          
+
           <Link
             style={{
               textDecoration: "none",
@@ -36,42 +38,6 @@ const LandingPage = () => {
           </Link>
         </div>
       </div>
-      {/* <div className={classes.main}>
-        <div className={classes.landingActions}>
-          <h1 className={classes.title}>Random Jokes Generator</h1>
-          <p className={classes.desc}>From dark humour to innocent puns.</p>
-        </div>
-        <Link
-          style={{
-            textDecoration: "none",
-            color: "white",
-            backgroundColor: "#318061",
-            padding: "1em 2.5em",
-            borderRadius: "7px",
-          }}
-          to="/funny"
-        >
-          Lets go
-        </Link>
-      </div>
-      <div className={classes.main}>
-        <div className={classes.landingActions}>
-          <h1 className={classes.title}>Random Cat Fact Generator</h1>
-          <p className={classes.desc}>Wanna find ourt more about cats?</p>
-        </div>
-        <Link
-          style={{
-            textDecoration: "none",
-            color: "white",
-            backgroundColor: "#318061",
-            padding: "1em 2.5em",
-            borderRadius: "7px",
-          }}
-          to="/cat-fact"
-        >
-          Lets go
-        </Link>
-      </div> */}
     </div>
   );
 };
